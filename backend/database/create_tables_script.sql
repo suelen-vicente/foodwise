931ff2d3a371b0931d6be1b482c3d4deac2052b6
suelen-vicente <suelen.blasques@gmail.com>
2023-11-22 18:27:06 -0500

    FR004 - Adding Database Tables

---
+++ backend/database/create_tables_script.sql
@@ -0,0 +1,24 @@
+-- Instructions:
+-- You need to run one piece each time. Running the entire file will result in error.
+--
+-- Create the ingredient table
+CREATE TABLE IF NOT exists ingredient (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    photo BYTEA -- BLOB data type for storing binary data
+);
+-- Create the recipe table
+CREATE TABLE IF NOT exists recipe (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    photo BYTEA, -- BLOB data type for storing binary data
+    steps TEXT NOT NULL
+);
+-- Create the junction table for the many-to-many relationship
+CREATE TABLE IF NOT exists recipe_ingredient (
+    recipe_id INTEGER REFERENCES recipe(id),
+    ingredient_id INTEGER REFERENCES ingredient(id),
+    PRIMARY KEY (recipe_id, ingredient_id)
+);
